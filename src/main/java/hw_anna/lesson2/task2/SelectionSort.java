d60095f4b530d12c3376d1d6a5545c59c8d643db
AnnaSavasteeva <anna.savasteeva@gmail.com>
2021-12-19 22:29:33 +0300

    Lesson 2, task 2

---
+++ src/main/java/hw_anna/lesson2/task2/SelectionSort.java
@@ -0,0 +1,28 @@
+package hw_anna.lesson2.task2;
+
+public class SelectionSort {
+
+    public static void sort(Integer[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int indexForMin = i;
+            int indexForMax = arr.length - 1 - i;
+
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[indexForMin]) {
+                    indexForMin = j;
+                }
+                if (arr[arr.length - 1 - j] > arr[indexForMax]) {
+                    indexForMax = arr.length - 1 - j;
+                }
+            }
+
+            int tempMin = arr[i];
+            arr[i] = arr[indexForMin];
+            arr[indexForMin] = tempMin;
+
+            int tempMax = arr[arr.length - 1 - i];
+            arr[arr.length - 1 - i] = arr[indexForMax];
+            arr[indexForMax] = tempMax;
+        }
+    }
+}
