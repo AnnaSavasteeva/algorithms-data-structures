23130aaa43106a7563300117464a0d6cfb9d9222
AnnaSavasteeva <anna.savasteeva@gmail.com>
2023-01-09 00:10:24 +0300

    hw 7. Add some changes

--- src/main/java/hw_anna/lesson7/Main.java
+++ src/main/java/hw_anna/lesson7/Main.java
@@ -1,30 +1,43 @@
 package hw_anna.lesson7;
 
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Main {
     private final static String MOSCOW = "Москва";
     private final static String VORONEZH = "Воронеж";
 
-    public static void main(String[] args) {
-        Map<Node, Node[]> cities = new LinkedHashMap<>(10);
-        fillCollectionWithCities(cities);
+    private static Node moscow;
+    private static Node toula;
+    private static Node razian;
+    private static Node kalouga;
+    private static Node lipetsk;
+    private static Node voronezh;
+    private static Node tambov;
+    private static Node saratov;
+    private static Node orel;
+    private static Node kursk;
 
-        Node[] moscowNeighbors = getMoscowNeighbors(cities);
+
+    public static void main(String[] args) {
+        Map<Node, List<Node.Neighbour>> cities = new LinkedHashMap<>(10);
+        makeGraph(cities);
+
+        List<Node.Neighbour> moscowNeighbors = getMoscowNeighbors(cities);
         if (moscowNeighbors != null) {
-            for (Node curCity : moscowNeighbors) {
+            for (Node.Neighbour currentCity : moscowNeighbors) {
                 StringBuilder wayToVoronezh = new StringBuilder();
                 wayToVoronezh.append(MOSCOW + " -> ");
-                getWayToVoronezh(curCity, cities, wayToVoronezh);
+                getWayToVoronezh(currentCity.getNeighbour(), cities, wayToVoronezh);
             }
         }
     }
 
-    private static void getWayToVoronezh(Node curCity, Map<Node, Node[]> cities, StringBuilder wayToVoronezh) {
-        if (curCity.isVisited()) return;
+    private static void getWayToVoronezh(Node currentCity, Map<Node, List<Node.Neighbour>> cities, StringBuilder wayToVoronezh) {
+        if (currentCity.isVisited()) return;
 
-        String cityName = curCity.getNodeName();
+        String cityName = currentCity.getNodeName();
         wayToVoronezh.append(cityName);
 
         if (cityName.equals(VORONEZH)) {
@@ -34,19 +47,19 @@ public class Main {
         }
 
         wayToVoronezh.append(" -> ");
-        curCity.setVisited(true);
+        currentCity.setVisited(true);
 
-        if (cities.containsKey(curCity)) {
-            Node[] citiesArr = cities.get(curCity);
-            if (citiesArr != null) {
-                for (Node city : citiesArr) {
-                    getWayToVoronezh(city, cities, wayToVoronezh);
+        if (cities.containsKey(currentCity)) {
+            List<Node.Neighbour> cityNeighbours = cities.get(currentCity);
+            if (cityNeighbours != null) {
+                for (Node.Neighbour neighbour : cityNeighbours) {
+                    getWayToVoronezh(neighbour.getNeighbour(), cities, wayToVoronezh);
                 }
             }
         }
     }
 
-    private static Node[] getMoscowNeighbors(Map<Node, Node[]> cities) {
+    private static List<Node.Neighbour> getMoscowNeighbors(Map<Node, List<Node.Neighbour>> cities) {
         for (Node keyCity : cities.keySet()) {
             if(keyCity.getNodeName().equals(MOSCOW)) {
                 return cities.get(keyCity);
@@ -55,27 +68,45 @@ public class Main {
         return null;
     }
 
-    private static void fillCollectionWithCities(Map<Node, Node[]> cities) {
-        Node moscow = new Node(MOSCOW);
-        Node toula = new Node("Тула");
-        Node razian = new Node("Рязань");
-        Node kalouga = new Node("Калуга");
-        Node lipetsk = new Node("Липецк");
-        Node voronezh = new Node(VORONEZH);
-        Node tambov = new Node("Тамбов");
-        Node saratov = new Node("Саратов");
-        Node orel = new Node("Орел");
-        Node kursk = new Node("Курск");
-
-        cities.put(moscow, new Node[]{toula, razian, kalouga});
-        cities.put(toula, new Node[]{lipetsk});
-        cities.put(lipetsk, new Node[]{voronezh});
+    private static void makeGraph(Map<Node, List<Node.Neighbour>> cities) {
+        createCities();
+        addNeighbours();
+        cities.put(moscow, moscow.getNeighbours());
+        cities.put(toula, toula.getNeighbours());
+        cities.put(lipetsk, lipetsk.getNeighbours());
         cities.put(voronezh, null);
-        cities.put(razian, new Node[]{tambov});
-        cities.put(tambov, new Node[]{saratov});
-        cities.put(saratov, new Node[]{voronezh});
-        cities.put(kalouga, new Node[]{orel});
-        cities.put(orel, new Node[]{kursk});
-        cities.put(kursk, new Node[]{voronezh});
+        cities.put(razian, razian.getNeighbours());
+        cities.put(tambov, tambov.getNeighbours());
+        cities.put(saratov, saratov.getNeighbours());
+        cities.put(kalouga, kalouga.getNeighbours());
+        cities.put(orel, orel.getNeighbours());
+        cities.put(kursk, kursk.getNeighbours());
+    }
+
+    private static void addNeighbours() {
+        moscow.setNeighbour(toula, 111);
+        moscow.setNeighbour(razian, 222);
+        moscow.setNeighbour(kalouga, 333);
+        toula.setNeighbour(lipetsk, 444);
+        lipetsk.setNeighbour(voronezh, 555);
+        razian.setNeighbour(tambov, 666);
+        tambov.setNeighbour(saratov, 777);
+        saratov.setNeighbour(voronezh, 888);
+        kalouga.setNeighbour(orel, 999);
+        orel.setNeighbour(kursk, 123);
+        kursk.setNeighbour(voronezh, 321);
+    }
+
+    private static void createCities() {
+        moscow = new Node(MOSCOW);
+        toula = new Node("Тула");
+        razian = new Node("Рязань");
+        kalouga = new Node("Калуга");
+        lipetsk = new Node("Липецк");
+        voronezh = new Node(VORONEZH);
+        tambov = new Node("Тамбов");
+        saratov = new Node("Саратов");
+        orel = new Node("Орел");
+        kursk = new Node("Курск");
     }
 }
--- src/main/java/hw_anna/lesson7/Node.java
+++ src/main/java/hw_anna/lesson7/Node.java
@@ -1,12 +1,24 @@
 package hw_anna.lesson7;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Node {
 
-    private String nodeName;
+    private final String nodeName;
     private boolean isVisited;
+    private final List<Neighbour> neighbours = new LinkedList<>();
 
     public Node(String nodeName) {
         this.nodeName = nodeName;
+    }
+
+    public void setNeighbour(Node neighbour, int distance) {
+        neighbours.add(new Neighbour(neighbour, distance));
+    }
+
+    public List<Neighbour> getNeighbours() {
+        return neighbours;
     }
 
     public String getNodeName() {
@@ -19,5 +31,23 @@ public class Node {
 
     public void setVisited(boolean visited) {
         isVisited = visited;
+    }
+
+    public static class Neighbour {
+        private final Node neighbour;
+        private final int distance;
+
+        public Neighbour(Node neighbour, int distance) {
+            this.neighbour = neighbour;
+            this.distance = distance;
+        }
+
+        public Node getNeighbour() {
+            return neighbour;
+        }
+
+        public int getDistance() {
+            return distance;
+        }
     }
 }
